b732e682698353185f258d2cd3487a9e2dcd54e5
strdnl <46500009+strdnl@users.noreply.github.com>
2020-11-17 19:39:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ return (a)}
 
 function build(k){
 bd=new BigUint64Array(65536);
-var pP=((2n**64n)-83n),m=2**16,tp,i=0,x=bd.length,ac=0n;
+var pP=((2n**64n)-83n),m=2**31,tp,i=0,x=bd.length,ac=0n;
 for (i in bd){
 	bd[i]=ac;
-	tp=BigInt(k[0]*(k[4]%m))%pP
+	tp=BigInt(k[0]*(k[4]%m-i))%pP
 	bd[i]=(bd[i]+tp)%pP;
-	tp=BigInt(k[1]*(k[5]%m-1))%pP
+	tp=BigInt(k[1]*(k[5]%m-i+1))%pP
 	bd[i]=(bd[i]+tp)%pP;
-	tp=BigInt(k[2]*(k[6]%m-3))%pP;
+	tp=BigInt(k[2]*(k[6]%m-i+3))%pP;
 	bd[i]=(bd[i]+tp)%pP;
-	tp=BigInt(k[3]*(k[7]%m-2))%pP;
+	tp=BigInt(k[3]*(k[7]%m-i+2))%pP;
 bd[i]=(bd[i]+tp)%pP;
 ac=bd[i]}
 return bd}
